5db43561fd2def94efff0bbf9042f93ed1c19105
Scott Mallinson <scott@scottmallinson.com>
2019-09-03 18:44:09 +0200

    Apply linting, code standards, and remove unnecessary code

--- serviceworker.js
+++ serviceworker.js
@@ -1,62 +1,54 @@
-const version = 'V0.07';
-const staticCacheName = version + 'staticfiles';
+const version = 'V0.08';
+const staticCacheName = `${version}staticfiles`;
 
-addEventListener('install', installEvent => {
+// eslint-disable-next-line no-restricted-globals
+addEventListener('install', (installEvent) => {
   installEvent.waitUntil(
     caches.open(staticCacheName)
-      .then(staticCache => {
-        // Nice to have
+      .then((staticCache) => {
         staticCache.addAll([
           '/img/android-chrome-192x192.png',
           '/img/android-chrome-512x512.png',
           '/img/apple-touch-icon.png',
           '/img/favicon-16x16.png',
           '/img/favicon-32x32.png',
-          '/img/favicon.ico'
-        ]); // end return addAll
-        // Must have
+          '/img/favicon.ico',
+        ]);
         return staticCache.addAll([
           '/img/tess.jpg',
-          '/offline.html'
-        ])
-      }) // end open then
-  ); // end waitUntil
-}); // end addEventListener
+          '/offline.html',
+        ]);
+      }),
+  );
+});
 
-addEventListener('activate', activateEvent => {
+// eslint-disable-next-line no-restricted-globals
+addEventListener('activate', (activateEvent) => {
   activateEvent.waitUntil(
     caches.keys()
-      .then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cacheName => {
-            if (cacheName != staticCacheName) {
-              return caches.delete(cacheName);
-            } // end if
-          }) // end map
-        ); // end return Promise.all
-      }) // end keys then
-      .then(() => {
-        return clients.claim();
-      }) // end then
-  ); // end waitUntil
-}); // end addEventListener
+      .then((cacheNames) => Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== staticCacheName) {
+            return caches.delete(cacheName);
+          }
+        }),
+      ))
+      // eslint-disable-next-line no-undef
+      .then(() => clients.claim()),
+  );
+});
 
-// When the browser requests a file...
-addEventListener('fetch', fetchEvent => {
-  const request = fetchEvent.request;
+// eslint-disable-next-line no-restricted-globals
+addEventListener('fetch', (fetchEvent) => {
+  const { request } = fetchEvent;
   fetchEvent.respondWith(
-    // First, look in the cache
     caches.match(request)
-      .then(responseFromCache => {
+      .then((responseFromCache) => {
         if (responseFromCache) {
           return responseFromCache;
-        } // end if
-        // Otherwise fetch from the network return fetch(request);
+        }
         return fetch(request)
-          .catch(error => {
-            // Show a fallback page instead
-            return caches.match('/offline.html');
-          }); // end fetch catch and return
-      }) // end match then
-  ); // end respondWith
-}); // end addEventListener
+          .catch(() => caches.match('/offline.html'));
+      }),
+  );
+});
